439599ccc73b01f0154aabd1f698245d0ee58cde
SatakeYusuke19920527 <satkyusk@icloud.com>
2022-11-24 22:00:06 +0900

    検索ボックス完成

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,14 @@
 import type { NextPage } from 'next';
 import Card from '../components/Card';
 import Layout from '../components/Layout';
+import Search from '../components/Search';
 import { sampleCards } from '../utils/sample';
 
 const Home:NextPage = () => {
   return (
     <Layout>
-      <div className="w-full  pt-12">
-        <h1 className="mb-8">
-          検索を配置予定
-        </h1>
+      <div className="w-full pt-8">
+        <Search />
         <div className="grid md:gap-6 mt-10 md:grid-cols-3 w-full my-12">
           {/* Card */}
           {sampleCards.map((page, index) => (
--- site.config.ts
+++ site.config.ts
@@ -5,3 +5,19 @@ export const siteConfig = {
   instagramUrl: "https://www.instagram.com/sat_yusuke/",
   githubUrl: "https://github.com/SatakeYusuke19920527"
 }
+
+export const searchConfig = {
+  category: "カテゴリ",
+  companyName: "企業名",
+  keyword: "キーワード",
+  search: "検索"
+}
+
+export const categoryConfig = [
+  { name: "成形設備" },
+  { name: "金型" },
+  { name: "成形屋" },
+  { name: "樹脂" },
+  { name: "展示会" },
+  {name: "その他"},
+]
