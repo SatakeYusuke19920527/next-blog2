92c7ddeec5b82db7acfe6d06adef82893bc37b22
SatakeYusuke19920527 <satkyusk@icloud.com>
2023-01-24 13:52:41 +0900

    favicon設定

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,47 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { Provider } from 'react-redux';
 import { store } from '../store/store';
 import '../styles/globals.css';
 
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/favicons/safari-pinned-tab.svg"
+          color="#000000"
+        />
+        <link rel="shortcut icon" href="/favicons/favicon.ico" />{' '}
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta
+          name="msapplication-config"
+          content="/favicons/browserconfig.xml"
+        />{' '}
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </>
   );
 }
